9216d0c72051656d52d6214da98cc3ee02a66012
Dustin Specker <dustinspecker@dustinspecker.com>
2015-11-03 19:51:48 -0600

    docs(src): add JSDoc for functions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import rework from 'rework';
 import reworkUrl from 'rework-plugin-url';
 import through from 'through2';
 
+/**
+ * Transforms URLs in files
+ * @param {String} filePath - path of CSS file that may be used by options.modify
+ * @param {String} fileContents - contents of the file at filePath
+ * @param {Object} [options] - rules for modifying URLs
+ * @param {String} [options.append] - URLs are appended with this value
+ * @param {Function} [options.modify] - This function is always called before append and prepend.
+ * @param {String} [options.prepend] - URLs are prepended with this value
+ * @return {String} - transformed URL
+ */
 function modifyUrls(filePath, fileContents, options) {
   return rework(fileContents)
     .use(reworkUrl(url => {
@@ -27,6 +37,11 @@ function modifyUrls(filePath, fileContents, options) {
     })).toString();
 }
 
+/**
+ * Pushes along files with transformed URLs
+ * @param {Object} options - same as described for modifyUrls function
+ * @return {Stream} - file with transformed URLs
+ */
 module.exports = function (options = {}) {
   return through.obj(function (file, enc, cb) {
     const modifiedContents = modifyUrls(file.path, file.contents.toString(), options);
